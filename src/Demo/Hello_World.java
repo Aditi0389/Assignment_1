e3b9a7ca50c18477c0947dcf144ed211bfadaa76
Aditi0389 <aditi03learning@gmail.com>
2023-09-15 21:09:45 +0530

    Initial comments

---
+++ src/Demo/Hello_World.java
@@ -0,0 +1,13 @@
+package Demo;
+
+public class Hello_World {
+	public static void main(String args[])
+	{
+		System.out.println("Hello World!");
+		int a = 10;
+		int b = 29;
+		System.out.println(a+b);
+	}
+
+
+}
