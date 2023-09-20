de1988f36baef46b3a9a70041193177f8d0422e3
Aditi0389 <aditi03learning@gmail.com>
2023-09-20 19:26:16 +0530

    Added a new file

---
+++ src/Demo/Number_Swap.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class Number_Swap {
+	public static void main(String args[])
+	{
+		int a = 10;
+		int b = 29;
+		System.out.println("a before swap is :"+a);
+		System.out.println("b before swap is :"+b);
+		a= a+b;
+		b = a-b;
+		a = a-b;
+		System.out.println("a after swap is :"+a);
+		System.out.println("b after swap is :"+b);
+		
+	}
+
+}
